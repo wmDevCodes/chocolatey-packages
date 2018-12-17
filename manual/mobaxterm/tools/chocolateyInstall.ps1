eadfcab5c9bf0a64dc38b45bfc068632760627ea
Werner Mayer <Werner@thinkit.co.nz>
2018-12-18 08:56:43 +1300

    version 10.9.0 to version 11.0.0 update

--- manual/mobaxterm/tools/chocolateyInstall.ps1
+++ manual/mobaxterm/tools/chocolateyInstall.ps1
@@ -1,20 +1,21 @@
 $ErrorActionPreference = 'Stop';
  
-$packageName = 'mobaxterm'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.mobatek.net/1092018073012523/MobaXterm_Installer_v10.9.zip'
-$url64      = 'https://download.mobatek.net/1092018073012523/MobaXterm_Installer_v10.9.zip'
+$packageName  = 'mobaxterm'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download.mobatek.net/1102018093083521/MobaXterm_Installer_v11.0.zip'
+$checkSum     = '789711D76EA0CF3D39B9A4B98FD66DD5A83B6E6309682B80C2B0FA57AF24E0C0'
+$checkSumType = 'SHA256'
  
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
-  url64bit      = $url64
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = $url
+  url64bit       = $url
 
-  checksum      = '66214550140D01FA5995AF52EFA4230C729E598981D4B5D9F99BF563B01B7C97'
-  checksumType  = 'sha256'
-  checksum64    = '66214550140D01FA5995AF52EFA4230C729E598981D4B5D9F99BF563B01B7C97'
-  checksumType64= 'sha256'
+  checksum       = $checkSum
+  checksumType   = $checkSumType
+  checksum64     = $checkSum
+  checksumType64 = $checkSumType
 }
  
 Install-ChocolateyZipPackage @packageArgs
@@ -22,8 +23,8 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'MSI'
-  file          = Join-Path $toolsDir 'MobaXterm_installer_10.9.msi'
-  file64        = Join-Path $toolsDir 'MobaXterm_installer_10.9.msi'
+  file          = Join-Path $toolsDir 'MobaXterm_installer_11.0.msi'
+  file64        = Join-Path $toolsDir 'MobaXterm_installer_11.0.msi'
  
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
