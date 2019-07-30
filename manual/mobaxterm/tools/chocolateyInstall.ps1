3feb746d2033cf8c8a966452e2682a928d334de4
wmDevCodes <devops@wmdev.codes>
2019-07-31 09:36:16 +1200

    version update

--- manual/mobaxterm/tools/chocolateyInstall.ps1
+++ manual/mobaxterm/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop';
  
 $packageName  = 'mobaxterm'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.mobatek.net/1112019010310554/MobaXterm_Installer_v11.1.zip'
-$checkSum     = '1E8898AB7A11A5AA9D8DA713E551E2805E53CF8FA7E33A169AF37FB32CE4221E'
+$url          = 'https://download.mobatek.net/1202019072610131/MobaXterm_Installer_v12.0.zip'
+$checkSum     = '39562C52BAB48344FBAA2C73F253A9FBF1C30860A3D4E997FFE89137C78D7887'
 $checkSumType = 'SHA256'
  
 $packageArgsZip = @{
@@ -22,8 +22,8 @@ Install-ChocolateyZipPackage @packageArgsZip
 $packapackageArgsMsigeArgs = @{
   packageName   = $packageName
   fileType      = 'MSI'
-  file          = Join-Path $toolsDir 'MobaXterm_installer_11.1.msi'
-  file64        = Join-Path $toolsDir 'MobaXterm_installer_11.1.msi'
+  file          = Join-Path $toolsDir 'MobaXterm_installer_12.0.msi'
+  file64        = Join-Path $toolsDir 'MobaXterm_installer_12.0.msi'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
