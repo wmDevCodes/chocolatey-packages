06dc8cf6ec136d406ed16827381a38007c81801b
wmDevCodes <devops@wmdev.codes>
2019-07-30 21:19:24 +1200

    code cleanup

--- manual/mobaxterm/tools/chocolateyInstall.ps1
+++ manual/mobaxterm/tools/chocolateyInstall.ps1
@@ -6,28 +6,26 @@ $url          = 'https://download.mobatek.net/1112019010310554/MobaXterm_Install
 $checkSum     = '1E8898AB7A11A5AA9D8DA713E551E2805E53CF8FA7E33A169AF37FB32CE4221E'
 $checkSumType = 'SHA256'
  
-$packageArgs = @{
+$packageArgsZip = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url            = $url
   url64bit       = $url
-
   checksum       = $checkSum
   checksumType   = $checkSumType
   checksum64     = $checkSum
   checksumType64 = $checkSumType
 }
  
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgsZip
  
-$packageArgs = @{
+$packapackageArgsMsigeArgs = @{
   packageName   = $packageName
   fileType      = 'MSI'
   file          = Join-Path $toolsDir 'MobaXterm_installer_11.1.msi'
   file64        = Join-Path $toolsDir 'MobaXterm_installer_11.1.msi'
- 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
  
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgsMsi
