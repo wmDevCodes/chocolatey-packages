95e47cde0bebf0ee02d210c2c0db102528a8fc3f
wmDevCodes <devops@wmdev.codes>
2019-07-30 21:19:35 +1200

    version update

--- manual/sqlyog/tools/chocolateyInstall.ps1
+++ manual/sqlyog/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
-﻿$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.3/SQLyog-13.1.3-0.x86Community.exe'
-$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.3/SQLyog-13.1.3-0.x64Community.exe'
+﻿$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.5/SQLyog-13.1.5-0.x86Community.exe'
+$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.5/SQLyog-13.1.5-0.x64Community.exe'
 
 $packageArgs = @{
   packageName    = 'sqlyog'
   fileType       = 'EXE'
   url            = $url
   url64bit       = $url64
-  checksum       = 'C8E9DC57DCB14CD10675251FA6C133A5CEE930449A6949A6011C40A29EF860FF'
+  checksum       = 'CCF0FE526E7B4931E8A76B7110E12DD8F705BD6AA666C03380B5B17329403101'
   checksumType   = 'SHA256'
-  checksum64     = '0429873305561121A85A6E0B55B202DF9AD3CCABD0D52168C09DF98D63E0799F'
+  checksum64     = 'E3071A8D0F24D8C2A45000D0F6A53912CFC06EE752CB4F623C1B4D54A27903A0'
   checksumType64 = 'SHA256'  
   silentArgs     = "/S"
 }
