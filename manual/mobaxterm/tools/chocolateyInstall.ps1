3c62f80cb82c83a327c687218f4963cd016b4fea
wmDevCodes <devops@wmdev.codes>
2020-02-17 08:08:49 +1300

    20.0 version update

--- manual/mobaxterm/tools/chocolateyInstall.ps1
+++ manual/mobaxterm/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
  
 $packageName  = 'mobaxterm'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.mobatek.net/1242019111120613/MobaXterm_Installer_v12.4.zip'
-$checkSum     = '3BAEF93B576B13D5410CED40BE03514B5CA35ECA63DC319EE412A343FB87E34F'
+$url          = 'https://download.mobatek.net/2002020012904840/MobaXterm_Installer_v20.0.zip'
+$checkSum     = '485FAA5BD27A4E9D4F08D8B55560BECE952D6F746BE8C0719BBCA8C31A19FA35'
 $checkSumType = 'SHA256'
-$MsiInstaller = 'MobaXterm_installer_12.4.msi'
+$MsiInstaller = 'MobaXterm_installer_20.0.msi'
  
 $packageArgsZip = @{
   packageName    = $packageName
